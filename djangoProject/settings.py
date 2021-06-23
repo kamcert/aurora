6fcde707d5d207b773edb8955717aaacefeaed7c
DESKTOP-HJ51S8E\hurricane <kamcert@kolon.com>
2021-06-23 23:16:49 +0900

    setting edit

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
-INSTALLED_APPS = {
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +39,7 @@ INSTALLED_APPS = {
     'django.contrib.staticfiles',
     'test',
     'rest_framework',
-}
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
